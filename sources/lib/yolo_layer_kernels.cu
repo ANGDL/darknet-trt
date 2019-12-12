2ad321d41680d98bbbee92dc9069ac55a91c4910
ANG <ang_z@outlook.com>
2019-12-12 10:31:34 +0800

    fix：修复yolo layer核函数中的索引计算错误

--- sources/lib/yolo_layer_kernels.cu
+++ sources/lib/yolo_layer_kernels.cu
@@ -3,42 +3,45 @@
 
 
 __device__
-float d_sigmod(float x) {
-	return 1.0f / (1.0 + __expf(x));
+inline float d_sigmod(const float& x) {
+	return 1.0f / (1.0f + __expf(-x));
 }
 
 __global__
 void kernel_yolo_layer(
 	const float* input,
 	float* output,
-	unsigned int grid_size,
-	unsigned int num_classes,
-	unsigned int num_boxes,
-	unsigned int output_size
+	const unsigned int grid_size,
+	const unsigned int num_classes,
+	const unsigned int num_boxes
 ) {
-	unsigned int id_x = threadIdx.x + blockDim.x * blockIdx.x;
-	unsigned int id_y = threadIdx.y + blockDim.y * blockIdx.y;
-	unsigned int id_z = threadIdx.z + blockDim.z * blockIdx.z;
+	unsigned int id_x = blockIdx.x * blockDim.x + threadIdx.x;
+	unsigned int id_y = blockIdx.y * blockDim.y + threadIdx.y;;
+	unsigned int id_z = blockIdx.z * blockDim.z + threadIdx.z;
 
-	if (id_x >= grid_size || id_y >= grid_size || id_z >= num_boxes) {
+	if ((id_x >= grid_size) || (id_y >= grid_size) || (id_z >= num_boxes)) {
 		return;
 	}
 
 	unsigned int num_grids = grid_size * grid_size;
 	unsigned int grid_idx = id_x + grid_size * id_y;
 
-	unsigned int loc_idx = grid_idx + num_grids * (id_z * (num_classes + 5));
-	output[loc_idx + 0] = d_sigmod(input[loc_idx + 0]);  // sigmod(tx)
-	output[loc_idx + 1] = d_sigmod(input[loc_idx + 1]);  // sigmod(ty)
-	output[loc_idx + 2] = __expf(input[loc_idx + 2]);  // exp(tw)
-	output[loc_idx + 3] = __expf(input[loc_idx + 3]);  // exp(th)
-	output[loc_idx + 4] = d_sigmod(input[loc_idx + 4]);  // sigmod(to)
+	output[grid_idx + num_grids * (id_z * (num_classes + 5) + 0)] = d_sigmod(input[grid_idx + num_grids * (id_z * (num_classes + 5) + 0)]);  // sigmod(tx)
 
-	for (cuuint32_t i = 0; i < num_boxes; ++i) {
-		output[loc_idx + 5 + i] = d_sigmod(input[loc_idx + 5 + i]);  // confidence score
+	output[grid_idx + num_grids * (id_z * (num_classes + 5) + 1)] = d_sigmod(input[grid_idx + num_grids * (id_z * (num_classes + 5) + 1)]); // sigmod(ty)
+
+	output[grid_idx + num_grids * (id_z * (num_classes + 5) + 2)] = __expf(input[grid_idx + num_grids * (id_z * (num_classes + 5) + 2)]);  // exp(tw)
+
+	output[grid_idx + num_grids * (id_z * (num_classes + 5) + 3)] = __expf(input[grid_idx + num_grids * (id_z * (num_classes + 5) + 3)]);  // exp(th) 
+
+	output[grid_idx + num_grids * (id_z * (num_classes + 5) + 4)] = d_sigmod(input[grid_idx + num_grids * (id_z * (num_classes + 5) + 4)]);  // sigmod(to)
+
+	for (unsigned int i = 0; i < num_classes; ++i)
+	{
+		output[grid_idx + num_grids * (id_z * (5 + num_classes) + (5 + i))]
+			= d_sigmod(input[grid_idx + num_grids * (id_z * (5 + num_classes) + (5 + i))]);
 	}
 }
-
 
 cudaError_t cuda_yolo_layer(
 	const void* input,
@@ -52,14 +55,17 @@ cudaError_t cuda_yolo_layer(
 ) {
 	assert(num_boxes == 3);
 	dim3 threads_per_blocks(16, 16, 4);
-	dim3 num_blocks(grid_size / threads_per_blocks.x, grid_size / threads_per_blocks.y, 1);
+	dim3 num_blocks(
+		grid_size / threads_per_blocks.x + 1,
+		grid_size / threads_per_blocks.y + 1,
+		num_boxes / threads_per_blocks.z + 1);
 
 	const float* input_f = reinterpret_cast<const float*>(input);
 	float* output_f = reinterpret_cast<float*>(output);
 
 	for (int i = 0; i < batch_size; ++i) {
-		kernel_yolo_layer << <num_blocks, threads_per_blocks >> > (
-			input_f, output_f, grid_size, num_classes, num_boxes, output_size);
+		kernel_yolo_layer << <num_blocks, threads_per_blocks, 0, stream >> > (
+			input_f + (i * output_size), output_f + (i * output_size), grid_size, num_classes, num_boxes);
 	}
 
 	return cudaGetLastError();
@@ -95,6 +101,6 @@ cudaError_t cuda_upsample_layer(const void* input, void* output, int batch_size,
 	int c, int h, int w, cudaStream_t stream)
 {
 	unsigned int size = w * h * c * batch_size * stride * stride;
-	kernel_upsample << <cuda_gridsize(size), KERNEL_BLOCK >> > (size, (float*)input, w, h, c, batch_size, stride, 1.0, (float*)output);
+	kernel_upsample << <cuda_gridsize(size), KERNEL_BLOCK, 0, stream >> > (size, (float*)input, w, h, c, batch_size, stride, 1.0, (float*)output);
 	return cudaGetLastError();
 }
