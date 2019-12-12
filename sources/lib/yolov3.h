ddb211c62a79ab76bc7d9c4393cd4e03d3fd6439
ANG <ang_z@outlook.com>
2019-12-12 10:36:27 +0800

    feature: 添加yolov3 infer

---
+++ sources/lib/yolov3.h
@@ -0,0 +1,25 @@
+﻿#ifndef _YOLOV3_H_
+#define _YOLOV3_H_
+
+#include "yolo.h"
+
+namespace darknet {
+	class YoloV3 : public Yolo
+	{
+	public:
+		explicit YoloV3(NetConfig* config, uint batch_size, float confidence_thresh, float nms_thresh);
+		void infer(const unsigned char* input);
+		std::vector<BBoxInfo> get_detecions(const int image_idx, const int image_w, const int image_h);
+
+		YoloV3Cfg* net_cfg;
+
+	private:
+		Tensor2BBoxes convert_to_bboxes;
+
+		int output_index_1;
+		int output_index_2;
+		int output_index_3;
+	};
+}
+
+#endif
