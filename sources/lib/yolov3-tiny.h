4fb895aae08123644c89b0ad7ac57b0447fc69c0
ANG <ang_z@outlook.com>
2019-12-12 10:35:53 +0800

    feature: 添加yolov3-tiny infer

---
+++ sources/lib/yolov3-tiny.h
@@ -0,0 +1,25 @@
+﻿#ifndef _YOLO_V3_TINY_H_
+#define _YOLO_V3_TINY_H_
+
+#include "yolo.h"
+
+namespace darknet {
+	class YoloV3Tiny : public Yolo
+	{
+	public:
+		explicit YoloV3Tiny(NetConfig* config, uint batch_size, float confidence_thresh, float nms_thresh);
+		void infer(const float* input);
+		std::vector<BBoxInfo> get_detecions(const int image_idx, const int image_h, const int image_w);
+
+		YoloV3TinyCfg* net_cfg;
+
+	private:
+		Tensor2BBoxes convert_to_bboxes;
+
+		int output_index_1;
+		int output_index_2;
+	};
+}
+
+
+#endif
